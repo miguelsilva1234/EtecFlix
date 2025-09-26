503879eb29cf90e1d7afa59e62384b9a2ae28b6c
miguelsilva1234 <161628496+miguelsilva1234@users.noreply.github.com>
2025-09-26 18:06:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Bem vindo ao EtecFlix</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nesciunt minima porro qui architecto, sequi repellat voluptatum dolorum esse fugiat nam aliquid eligendi quaerat in quidem. Eaque iste tempora reprehenderit!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque quisquam unde accusamus voluptatibus repellat provident? Ullam ad soluta blanditiis, dolorem explicabo ea modi nesciunt cupiditate, vel dolor, omnis odio ipsum.</p>
+    <button>Botao</button>
+</body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sobre o projeto</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti iure ab quidem similique amet eligendi libero, iusto enim voluptatem, praesentium doloribus repellendus et laudantium nobis nesciunt aperiam nisi optio? Dolorum.</p>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa magni voluptatum, sunt quibusdam nesciunt sed deserunt earum consectetur assumenda adipisci! Aut ipsum, earum quod a asperiores velit enim facere libero.</p>
+    <ul>
+        <li>aaaa</li>
+        <li>aaa</li>
+        <li>aaa</li>
+        <li>aaa</li>
+    </ul>
+</body>
+</html>
