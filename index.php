53cf2a5b06fbc89e167d58e85db90de92dfc6712
miguelsilva1234 <miguel.silva233@etec.sp.gov.br>
2025-10-02 23:00:24 -0300

    ta indo

--- index.php
+++ index.php
@@ -4,15 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Meu Sistema</title>
+    <title>EtecFlix</title>
 </head>
 <body>
+    
     <?php include 'header.php';?>
 
     <div class="container">
-        <h1>Review de filmes</h1>
-
+        <div class="card">
+            <h1>EtecFlix</h1>
+            <h2>Review de filmes</h2>
+            <div class="btn-group" role="group" aria-label="Basic   mixed styles example">
+                <a href="login.php" type="button" class="btn btn-danger">Login</a>
+            </div>
+        </div>
     </div>
+
+
+    <?php include 'rodape.php'; ?>
 
 </body>
 </html>
