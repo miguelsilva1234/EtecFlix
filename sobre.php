8c7dc753a21c0f93fb8fbefec3bb507311e9df6f
miguelsilva1234 <161628496+miguelsilva1234@users.noreply.github.com>
2025-10-29 14:37:55 -0300

    Add files via upload

--- sobre.php
+++ sobre.php
@@ -3,18 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sobre.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Sobre o projeto</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti iure ab quidem similique amet eligendi libero, iusto enim voluptatem, praesentium doloribus repellendus et laudantium nobis nesciunt aperiam nisi optio? Dolorum.</p>
-    <hr>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa magni voluptatum, sunt quibusdam nesciunt sed deserunt earum consectetur assumenda adipisci! Aut ipsum, earum quod a asperiores velit enim facere libero.</p>
-    <ul>
-        <li>aaaa</li>
-        <li>aaa</li>
-        <li>aaa</li>
-        <li>aaa</li>
-    </ul>
+    
+    <?php include 'header.php';?>
+
+    <div class="container">
+        <div class="card">
+            <h1>Sobre o Projeto</h1>
+            <p>Nosso projeto é um site dedicado a amantes de entretenimento, onde você pode avaliar, comentar e compartilhar suas opiniões sobre filmes, séries e livros. A ideia é criar uma comunidade colaborativa, onde cada usuário contribui com suas resenhas e recomendações, ajudando outros a descobrir novas histórias e experiências.
+
+            Além das avaliações, o site oferece um espaço para discussões e trocas de ideias, permitindo que você interaja com outros fãs, compare pontos de vista e explore diferentes gêneros.
+
+            Nosso objetivo é transformar a maneira como as pessoas consomem conteúdo, incentivando o diálogo, a crítica construtiva e a paixão pela arte. Seja você um cinéfilo, um viciado em séries ou um leitor assíduo, aqui é o lugar ideal para compartilhar o que pensa e descobrir o que o mundo está comentando.</p>
+            <div class="btn-group" role="group" aria-label="Basic   mixed styles example">
+               <p>Integrantes do Projeto:</p>
+               <img src="img/53174188191_42d4c831ae_o.jpg" alt="">
+               <img src="" alt="">
+               <img src="" alt="">
+               <img src="" alt="">
+               <img src="" alt="">
+            </div>
+        </div>
+    </div>
+
+
+    <?php include 'rodape.php'; ?>
+
 </body>
 </html>
